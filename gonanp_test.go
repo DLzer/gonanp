db19716b4e5d8241df26a44dd2c715dfec74b5cf
DLzer <dillon.lomnitzer@gmail.com>
2024-12-16 12:42:29 -0500

    added: test comments

--- gonanp_test.go
+++ gonanp_test.go
@@ -9,6 +9,10 @@ import (
 )
 
 func TestGenerateNanp(t *testing.T) {
+	// Regex Asserts NANP
+	// Where NANP numbers are ten digits in length
+	// and they are in the format NXX-NXX-XXXX
+	// where N is any digit 2-9 and X is any digit 0-9
 	reg := regexp.MustCompile(`^\(?([2-9][0-8][0-9])\)?[-.●]?([2-9][0-9]{2})[-.●]?([0-9]{4})$`)
 
 	for i := 0; i < 100; i++ {
